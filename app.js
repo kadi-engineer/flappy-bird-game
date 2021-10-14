6bb610faedde424e9c65869b9899966c6bdea3df
Kadijah <kadijah.1.jones2@gmail.com>
2021-10-14 00:48:04 -0400

    updated

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('COMContentLoaded', () => {
+    const bird = document.querySelector('.bird')
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground')
+
+
+})
